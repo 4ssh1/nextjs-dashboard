48d8e03daa381986ca10fa230abbb51f1b9fe1ce
Nzeshi Sarah <sarahnzeshi05@gmail.com>
2025-09-12 22:30:40 +0100

    added fonts file

---
+++ app/ui/fonts.tsx
@@ -0,0 +1,12 @@
+
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({
+  subsets: ['latin'],
+  // you could add more options like weight, style etc as needed
+});
+
+export const lusitana = Lusitana({
+  weight: ['400', '700'],     
+  subsets: ['latin'],
+});
