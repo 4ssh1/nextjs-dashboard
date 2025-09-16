da1e9c6104acea8127c1408d5e99d1ade5e4f6e2
Nzeshi Sarah <sarahnzeshi05@gmail.com>
2025-09-16 22:45:34 +0100

    done with layout

---
+++ app/dashboard/customer/page.tsx
@@ -0,0 +1,10 @@
+
+function page() {
+  return (
+    <div>
+      <p>Customer page</p>
+    </div>
+  )
+}
+
+export default page
--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,9 +1,8 @@
-
 import { Inter, Lusitana } from 'next/font/google';
+
 
 export const inter = Inter({
   subsets: ['latin'],
-  // you could add more options like weight, style etc as needed
 });
 
 export const lusitana = Lusitana({
