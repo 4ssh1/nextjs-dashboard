e5c7e31f6594ecd99809ac4245016a6d00e43576
Nzeshi Sarah <sarahnzeshi05@gmail.com>
2025-10-03 13:16:55 +0100

    added meta data

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -72,9 +72,6 @@ export default function LoginForm() {
           Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
         </Button>
 
-        <Button className="mt-4 w-full">
-          Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
-        </Button>
         <div className="flex h-8 items-end space-x-1">
           {errorMessage && (
             <>
