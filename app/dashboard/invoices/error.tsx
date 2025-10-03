82441fb5b9b5d8a384f55acb5c9ed9fbeb0912de
Nzeshi Sarah <sarahnzeshi05@gmail.com>
2025-10-03 12:18:07 +0100

    improved accessibilty and added eslint

--- app/dashboard/invoices/error.tsx
+++ app/dashboard/invoices/error.tsx
@@ -28,3 +28,7 @@ export default function Error({
     </main>
   );
 }
+
+/**
+ * The `error.tsx` file serves as a catch-all for unexpected errors and allows you to display a fallback UI to your users
+ */
